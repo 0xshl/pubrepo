cf6e9a24314a7f79eb4b95bf788441fe0d8d2916
Kali Live user <web20502050@gmail.com>
2022-02-01 17:33:26 +0000

    cleared

--- https___developers_oppomobile_com_static_scripts_error_server_js.js
+++
@@ -1,6 +0,0 @@
-if(document.getElementsByTagName('nav').length==0){
-    document.body.style.background='#f6f6f6';
-    document.getElementsByClassName('server-error')[0].style.cssText='text-align: center;padding-top: 200px;';
-    document.getElementsByClassName('msg-title')[0].style.cssText='color: #3a4049;font-size: 30px;';
-    document.getElementsByClassName('msg-body')[0].style.cssText='font-size: 16px;color: #6e7277;';
-}
